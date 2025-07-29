d4dd8d145a21d51efaabff4a5731819c94575a50
mattstief <mstieferman@gmail.com>
2025-07-29 14:27:07 -0500

    formatting

--- api-gateway-function-nosql-table/terraform/outputs.tf
+++ api-gateway-function-nosql-table/terraform/outputs.tf
@@ -1,4 +1,4 @@
-output "repository_url" {
+output "repository_path" {
   description = "The full URL for pushing images to the OCIR repository."
   value       = "${lower(data.oci_identity_regions.current.regions[0].key)}.ocir.io/${data.oci_objectstorage_namespace.this.namespace}/${var.container_repository_name}"
 }
