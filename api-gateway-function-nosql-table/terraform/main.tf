cc3400f02ac09b91363f38ff1fe4a33deab8ef58
mattstief <mstieferman@gmail.com>
2025-07-29 14:14:09 -0500

    repo output

--- api-gateway-function-nosql-table/terraform/main.tf
+++ api-gateway-function-nosql-table/terraform/main.tf
@@ -62,8 +62,3 @@ module "container_repository" {
   compartment_id            = var.compartment_ocid
   container_repository_name = var.container_repository_name
 } 
-
-output "repository_url" {
-  description = "The full URL for pushing images to the OCIR repository."
-  value       = "${lower(data.oci_identity_regions.current.regions[0].key)}.ocir.io/${data.oci_objectstorage_namespace.this.namespace}/${var.container_repository_name}"
-}
